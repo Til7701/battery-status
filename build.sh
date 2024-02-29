aca36cc660f90b1b4f2832247ecc1749d382880d
Tilman Holube <tilman@holube.de>
2024-02-29 08:15:36 +0100

    jpackage build

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+echo "Building client jar"
+mvn --batch-mode --update-snapshots clean install package
+
+echo "Running jpackage"
+jpackage --type exe \
+--verbose \
+--input "./target" \
+--main-jar "./target/battery-status.jar" \
+--resource-dir "./jpackage" \
+--name "battery-status" \
+--app-version "2.0.0" \
+--dest "./target/dist" \
+--win-upgrade-uuid "19c20ba8-f49e-43e7-8efe-40970f2be007"
+
+exit 0
